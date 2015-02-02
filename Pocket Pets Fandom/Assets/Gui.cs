23090565c9ddec655b9d9c9f1e8e96a6923381f2
ndeko <ndeko12@gmail.com>
2015-02-01 18:02:07 -0800

    Initial commit testing
    First try

---
+++ Pocket Pets Fandom/Assets/Gui.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public GameObject myPet;
+	public Texture2D bgImage;
+	public Texture2D fgImage;
+	public GUIStyle stats = new GUIStyle ();
+	public GUIStyle btns = new GUIStyle ();
+	float barSize;
+	float barSize2;
+
+	//the GUI scale ratio  
+	private float guiRatio; 
+	private float sHeight; 
+	private Vector3 GUIsF;
+
+	void Start ()
+	{
+		myPet = GameObject.FindGameObjectWithTag("Pet");
+		barSize = 150; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		barSize = myPet.GetComponent<Pet>().hunger;
+		barSize2 = myPet.GetComponent<Pet>().happy;
+
+		//get the screen's width  
+		sHeight = Screen.height;  
+		//calculate the scale ratio  
+		guiRatio = sHeight/600;  
+		//create a scale Vector3 with the above ratio  
+		GUIsF = new Vector3(guiRatio,guiRatio,1);
+
+	}
+
+	void OnGUI()
+	{
+		GUI.matrix = Matrix4x4.TRS(new Vector3(Screen.width/2 - Screen.width/2*GUIsF.x, Screen.height - Screen.height*GUIsF.y,0),Quaternion.identity,GUIsF);
+
+
+		GUI.BeginGroup (new Rect (Screen.width/2 - 200, Screen.height - 80, 150, 40));
+		GUI.DrawTexture (new Rect (0, 0, 150, 40), bgImage);
+		GUI.BeginGroup (new Rect (0, 0, barSize, 40));
+		GUI.DrawTexture (new Rect (0, 0, 150, 40), fgImage);
+		GUI.EndGroup ();
+		GUI.EndGroup ();
+		GUI.Label (new Rect (Screen.width/2 - 200, Screen.height - 80, 150, 40), "Hunger", stats);
+
+		GUI.BeginGroup (new Rect (Screen.width/2 + 50, Screen.height - 80, 150, 40));
+		GUI.DrawTexture (new Rect (0, 0, 150, 40), bgImage);
+		GUI.BeginGroup (new Rect (0, 0, barSize2, 40));
+		GUI.DrawTexture (new Rect (0, 0, 150, 40), fgImage);
+		GUI.EndGroup ();
+		GUI.EndGroup ();
+		GUI.Label (new Rect (Screen.width/2 + 50, Screen.height - 80, 150, 40), "Happy", stats);
+
+
+		GUI.matrix = Matrix4x4.TRS (new Vector3 (Screen.width / 2 - Screen.width / 2 * GUIsF.x, GUIsF.y, 0), Quaternion.identity, GUIsF);
+
+
+
+		if(GUI.Button(new Rect(10, 10, 200, 100), "Feed", btns))
+		{
+			myPet.GetComponent<Pet>().Feed();
+		}
+		if(GUI.Button(new Rect(Screen.width - 210, 10, 200, 100), "Play", btns))
+		{
+			myPet.GetComponent<Pet>().Play();
+		}
+
+		/*
+		if(GUI.Button(new Rect(Screen.width - 105, Screen.height - 55, 100, 50), "Save", btns))
+		{
+			myPet.GetComponent<SavingAndLoading>().SaveData(myPet);
+		}
+		*/
+	}
+}
---
+++ Pocket Pets Fandom/Assets/SavingAndLoading.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+public class SavingAndLoading : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void loadOrSetup(GameObject currentPet)
+	{
+		if(PlayerPrefs.HasKey("hunger") && PlayerPrefs.HasKey("happy"))
+		{
+			LoadData(currentPet);
+		}
+		else
+		{
+			//fix later
+			Setup(currentPet, 150);
+		}
+	}
+
+
+	public void Setup(GameObject currentPet, float Max)
+	{
+		if(PlayerPrefs.HasKey("hunger") && PlayerPrefs.HasKey("happy"))
+		{
+			 currentPet.GetComponent<Pet>().hunger = PlayerPrefs.GetFloat("hunger");
+			currentPet.GetComponent<Pet>().happy = PlayerPrefs.GetFloat("happy" );
+		}
+		else
+		{
+			currentPet.GetComponent<Pet>().hunger = Max;
+			PlayerPrefs.SetFloat("hunger", Max);
+
+			currentPet.GetComponent<Pet>().happy = Max;
+			PlayerPrefs.SetFloat("happy", Max);
+		}
+	}
+
+
+
+	public void SaveData(GameObject currentPet)
+	{
+		PlayerPrefs.SetFloat("hunger", currentPet.GetComponent<Pet>().hunger);
+		PlayerPrefs.SetFloat("happy", currentPet.GetComponent<Pet>().happy);
+		PlayerPrefs.SetString("timeSinceLastPlay", System.DateTime.Now.ToString());
+		PlayerPrefs.Save();
+	}
+
+
+
+	public void LoadData(GameObject currentPet)
+	{
+			DateTime lastPlay = Convert.ToDateTime(PlayerPrefs.GetString("timeSinceLastPlay"));
+			TimeSpan timeDifference = DateTime.Now.Subtract(lastPlay);
+			int difference = Convert.ToInt32(timeDifference.TotalSeconds);
+			currentPet.GetComponent<Pet>().hunger = (PlayerPrefs.GetFloat("hunger") - ((float)difference / 10));
+			currentPet.GetComponent<Pet>().happy = PlayerPrefs.GetFloat("happy") - ((float)difference / 10);
+	}
+
+
+
+	public void OnApplicationQuit()
+	{
+		SaveData(this.gameObject);
+	}
+
+}
